a847a9ad7f4ffaaf83153c98c9bfb5fb7c3ae59d
ARPO <3423211241@qq.com>
2025-11-23 00:36:21 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- 添加这一行，使用相对路径
+  base: '/Midi-visitor/', // <--- 添加这一行，使用相对路径
 })
